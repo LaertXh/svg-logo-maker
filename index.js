e4c3ba59512b7f2d33830e86e3e337c7fb590a55
Laert Xhumari <laertxhumari@gmail.com>
2023-03-08 19:46:48 -0500

    reworked some of the classes and fully implemented the index

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
-//DEPENDENCIES 
+//DEPENDENCIES
 const inquirer = require("inquirer");
-//DATA
+const fs = require("fs");
+const Circle = require("./lib/Circle.js");
+const Square = require("./lib/Square.js");
+const Triangle = require("./lib/Triangle.js");
 
-//FUNCTIONS
-
-//USER INTERACTIONS
-
-//INITIALIZATIONS
-inquirer.prompt([
+// INITIALIZATIONS
+inquirer
+  .prompt([
     {
-        type: 'text',
-        message: 'Hello',
-        name: 'q1'
-    }
+      type: "input",
+      message: "What text should the logo display? Three letters max.",
+      name: "text",
+      validate: function (input) {
+        if (input.length > 3) {
+          return "Logo text should be at most 3 characters. Try again";
+        }
+        return true;
+      },
+    },
+    {
+      type: "input",
+      message: "What color should the text be?",
+      name: "textColor",
+    },
+    {
+      type: "input",
+      message: "What color should the background be?",
+      name: "bgColor",
+    },
+    {
+      type: "list",
+      message: "Choose a shape from the given list.",
+      name: "shapes",
+      choices: ["Triangle", "Circle", "Square"],
+    },
+    {
+      type: "input",
+      message: "What background color should the shape have?",
+      name: "shapesColor",
+    },
+  ])
+  .then((response) => {
+    console.log("Your inputs were recorded");
+    let shape;
 
-]).then((response) => {
-    console.log("Success");
-})
+    if (response.shapes === "Triangle") shape = new Triangle();
+    else if (response.shapes === "Square") shape = new Square();
+    else shape = new Circle();
+    shape.setShapeColor(response.shapesColor);
 
-
-
-const str = 
-`<svg version="1.1"
-width="${Shape.width}" height="${Shape.height}"
+    const str = `<svg version="1.1"
+width="${shape.width}" height="${shape.height}"
 xmlns="http://www.w3.org/2000/svg">
 
-<rect width="100%" height="100%" fill="${Shape.color}" />
+<rect width="100%" height="100%" fill="${response.bgColor}" />
 
-<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />
+${shape.render()}
 
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="${Shape.textColor}">SVG</text>
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="${
+      response.textColor
+    }">${response.text}</text>
 
-</svg>`
+</svg>`;
+
+    fs.writeFile("test.svg", str, (err) =>
+      err ? console.log(err) : console.log("Logo is now generated")
+    );
+  });
